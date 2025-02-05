f0d761407a0b4a2cf0232e3d01d09a1089930d2a
shiljiraj <78131334+shiljiraj@users.noreply.github.com>
2025-02-05 23:10:45 +0530

    Update Pro_run.java

--- src/Pro_run.java
+++ src/Pro_run.java
@@ -4,7 +4,7 @@ public class Pro_run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello girl");
+		System.out.println("hello girl + chinnu");
 
 	}
 
